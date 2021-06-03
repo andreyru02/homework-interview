e71cad571c8ae41b4edb611a9c4d8725e7bd1bf3
Andrey Chebunin <andrey.tchebunin@yandex.ru>
2021-06-03 17:02:13 +0800

    step 1

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        if len(self.stack) == 0:
+            return True
+        else:
+            return False
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def peek(self):
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+
+if __name__ == '__main__':
+    stack = Stack()
